15b6fd89159872e761fae7a1a16a45d6e943e312
Douglas Calaça <SEUEMAIL@gmail.com>
2025-09-08 20:40:35 -0300

    fix: Remover pasta mobile do build para corrigir erro de compilação

--- mobile/src/services/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://kfsteismyqpekbaqwuez.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtmc3RlaXNteXFwZWtiYXF3dWV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0ODU4NDMsImV4cCI6MjA2NTA2MTg0M30.nuHieAbGz65Lm5KlNamxO_HS_SFy0DGm6tIIbty7Z8A';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
