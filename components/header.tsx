e48dcf123bb7ee0a4dd95384d2cd7810463fb60a
Douglas Calaça <SEUEMAIL@gmail.com>
2025-10-15 20:03:20 -0300

    fix: usar logooriginal.png no header conforme solicitado

--- components/header.tsx
+++ components/header.tsx
@@ -61,7 +61,7 @@ export function Header() {
             <div className="flex items-center">
               <Link href="/" className="flex items-center">
                 <Image
-                  src="/logoresumo.png"
+                  src="/logooriginal.png"
                   alt="CalcFy - Calculadoras Financeiras Gratuitas"
                   width={120}
                   height={40}
@@ -108,7 +108,7 @@ export function Header() {
             <div className="flex items-center">
               <Link href="/" className="flex items-center">
                 <Image
-                  src="/logoresumo.png"
+                  src="/logooriginal.png"
                   alt="CalcFy - Calculadoras Financeiras Gratuitas"
                   width={120}
                   height={40}
